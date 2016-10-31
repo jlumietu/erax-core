d21e9be8b3bc5ebaf90053ab60159cedba32cb87
Mikel Ibiricu <jlumietu@gmail.com>
2016-10-31 14:30:20 +0100

    PrincipalSerializableIdLocator interface

---
+++ erax-core/src/main/java/com/erax/principal/PrincipalSerializableIdLocator.java
@@ -0,0 +1,12 @@
+package com.erax.principal;
+
+/**
+ * 
+ * @author DOIBALMI
+ *
+ */
+public interface PrincipalSerializableIdLocator {
+	
+	public abstract PrincipalSerializableId findPrincipalSerializableId();
+
+}
