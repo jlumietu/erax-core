cbe7e299f4b8709710558551a5c97154554ed550
Mikel Ibiricu <jlumietu@gmail.com>
2016-11-02 12:18:22 +0100

    Commit, once and for all

---
+++ erax-core/src/main/java/com/erax/principal/PrincipalSerializableId.java
@@ -0,0 +1,14 @@
+package com.erax.principal;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author jlumietu
+ *
+ */
+public interface PrincipalSerializableId {
+
+	public abstract Serializable getId();
+
+}
---
+++ erax-core/src/main/java/com/erax/principal/PrincipalSerializableIdLocator.java
@@ -0,0 +1,12 @@
+package com.erax.principal;
+
+/**
+ * 
+ * @author jlumietu
+ *
+ */
+public interface PrincipalSerializableIdLocator {
+	
+	public abstract PrincipalSerializableId findPrincipalSerializableId();
+
+}
